13ec6a0d1daa9462698c57efbfdb036d97b5c5b3
FatChicken277 <1637@holbertonschool.com>
2020-08-17 13:51:29 -0500

    added comments

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""This module contains a function that generates a .tgz 
+    archive from the contents of the web_static.
+"""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
+    """Fenerates a .tgz archive from the contents of the web_static.
+
+    Returns:
+        return the archive path if the archive has been correctly generated.
+        Otherwise, it should return None.
+    """
     t = datetime.now()
     formated = "{}{}{}{}{}".format(t.year, t.month, t.day, t.minute, t.second)
     file = "versions/web_static_{}.tgz".format(formated)
