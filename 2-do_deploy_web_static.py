5340272d49c5864bcf865451b579e2a1ca71ee39
FatChicken277 <1637@holbertonschool.com>
2020-08-17 15:26:22 -0500

    added 2-do_deploy_web_static.py file

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""This module contains functions that can be used to
+    generates a .tgz archive from the contents of the web_static and
+    distributes an archive to your web servers
+"""
+from fabric.api import local, env, settings
+from fabric.operations import run, put
+from datetime import datetime
+from os import path
+
+env.hosts = ['35.196.35.103', '34.75.67.159']
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static.
+
+    Returns:
+        return the archive path if the archive has been correctly generated.
+        Otherwise, it should return None.
+    """
+    t = datetime.now()
+    formated = "{}{}{}{}{}{}".format(t.year, t.month, t.day,
+                                     t.hour, t.minute, t.second)
+    file = "versions/web_static_{}.tgz".format(formated)
+
+    local('mkdir -p versions')
+    r = local('tar -czvf {} web_static/'.format(file))
+
+    if r == 0:
+        return file
+    return None
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to your web servers.
+
+    Args:
+        archive_path (str): tgz path.
+
+    Returns:
+        boolean: true if successful and false otherwise.
+    """
+
+    noext = archive_path.replace(".tgz", "").split("/")[1]
+    if not path.exists(archive_path):
+        return False
+
+    try:
+        put('{}'.format(archive_path), '/tmp/')
+        archive_path = archive_path.split("/")[1]
+
+        server_dir = "/data/web_static/releases/{}".format(noext)
+        run('mkdir -p {}'.format(server_dir))
+
+        run(' tar -xzf /tmp/{} -C {}'.format(archive_path, server_dir))
+        run('rm /tmp/{}'.format(archive_path))
+        run('mv {}/web_static/* {}/'.format(server_dir, server_dir))
+        run('rm -rf {}/web_static'.format(server_dir))
+        run('unlink /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(server_dir))
+    except Exception:
+        return False
+    return True
