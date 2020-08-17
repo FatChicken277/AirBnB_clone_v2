0034ce2499a186058532649bbaa22c956c6358b6
FatChicken277 <1637@holbertonschool.com>
2020-08-17 12:19:50 -0500

    added 0-setup_web_static.sh file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# This script sets up your web servers for the deployment of web_static.
+
+# Install Nginx if it not already installed.
+
+if ! command -v nginx &> /dev/null; then
+    apt-get update
+    apt-get -y install nginx
+fi
+
+# Create the necessary folders if they don't exist.
+
+create_folder () {
+    if [ ! -d "$1" ]; then
+        mkdir "$1"
+    fi
+}
+
+create_folder /data/
+create_folder /data/web_static/
+create_folder /data/web_static/releases/
+create_folder /data/web_static/shared/
+create_folder /data/web_static/releases/test/
+
+# Create a fake HTML file
+
+text="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
+echo -e "$text"  > /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+# (If the symbolic link already exists, it should be deleted and recreated)
+
+symbolic_link="/data/web_static/current"
+
+if [ -L "$symbolic_link" ]; then
+    unlink "$symbolic_link"
+fi
+
+ln -s /data/web_static/releases/test/ "$symbolic_link"
+
+# Change ownership and group of files.
+
+chown -R ubuntu:ubuntu /data/
+
+# Chnage Nginx configuration.
+
+file="/etc/nginx/sites-available/default"
+text="server {\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n"
+
+if ! grep -Fq "location /hbnb_static {" "$file"; then
+    sed -i "0,/^server {/s//$text/" "$file"
+fi
+
+service nginx restart
