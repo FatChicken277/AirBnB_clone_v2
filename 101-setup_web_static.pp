48cba6417312001ceb901fac252bfac199108724
FatChicken277 <1637@holbertonschool.com>
2020-08-17 22:09:50 -0500

    change file for exec

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -12,55 +12,21 @@ package { 'nginx':
 
 # create files drwxr-xr-x
 
-file { '/data':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => Package['nginx'],
+exec { 'shared':
+  command => 'mkdir -p /data/web_static/shared/',
+  path    => ['/usr/bin', '/bin'],
 }
 
-file { '/data/web_static':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data'],
-}
-
-file { '/data/web_static/releases':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static'],
-}
-
-file { '/data/web_static/shared':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static'],
-}
-
-file { '/data/web_static/releases/test':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static/releases'],
+exec { 'test':
+  command => 'mkdir -p /data/web_static/releases/test/',
+  path    => ['/usr/bin', '/bin'],
 }
 
 # html
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => file,
-  mode    => '0755',
   content => "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n",
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static/releases/test'],
 }
 
 # create symbolic link
@@ -68,10 +34,13 @@ file { '/data/web_static/releases/test/index.html':
 file { '/data/web_static/current':
   ensure  => link,
   target  => '/data/web_static/releases/test',
-  mode    => '0777',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static', '/data/web_static/releases/test']
+}
+
+# Change ownership and group of files.
+
+exec { 'owner_group':
+  command => 'chown -R ubuntu:ubuntu /data/',
+  path    => ['/usr/bin', '/bin'],
 }
 
 # config
@@ -79,7 +48,7 @@ file { '/data/web_static/current':
 file_line { 'config_file':
   path    => '/etc/nginx/sites-available/default',
   line    => "\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n",
-  after   => 'listen 80 default_server;',
+  after   => '^server {',
   require => Package['nginx'],
 }
 
