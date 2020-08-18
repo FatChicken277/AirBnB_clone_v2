6adf2a38b47b5be989dc73eada0a41709366b7bf
FatChicken277 <1637@holbertonschool.com>
2020-08-17 21:59:08 -0500

    added more stuff

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,38 +1,55 @@
+# install nginx
+
+exec { 'update':
+  command => 'sudo apt-get update',
+  path    => ['/usr/bin', '/bin'],
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update'],
+}
+
 # create files drwxr-xr-x
 
-file { '/data/':
-  ensure => directory,
-  mode   => '0755',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+file { '/data':
+  ensure  => directory,
+  mode    => '0755',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => Package['nginx'],
 }
 
 file { '/data/web_static':
-  ensure => directory,
-  mode   => '0755',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure  => directory,
+  mode    => '0755',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data'],
 }
 
-file { '/data/web_static/releases/':
-  ensure => directory,
-  mode   => '0755',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+file { '/data/web_static/releases':
+  ensure  => directory,
+  mode    => '0755',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data/web_static'],
 }
 
-file { '/data/web_static/shared/':
-  ensure => directory,
-  mode   => '0755',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+file { '/data/web_static/shared':
+  ensure  => directory,
+  mode    => '0755',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data/web_static'],
 }
 
-file { '/data/web_static/releases/test/':
-  ensure => directory,
-  mode   => '0755',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+file { '/data/web_static/releases/test':
+  ensure  => directory,
+  mode    => '0755',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data/web_static/releases'],
 }
 
 # html
@@ -41,18 +58,20 @@ file { '/data/web_static/releases/test/index.html':
   ensure  => file,
   mode    => '0755',
   content => "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n",
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data/web_static/releases/test'],
 }
 
 # create symbolic link
 
 file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test',
-  mode   => '0777',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure  => link,
+  target  => '/data/web_static/releases/test',
+  mode    => '0777',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data/web_static', '/data/web_static/releases/test']
 }
 
 # config
@@ -61,6 +80,7 @@ file_line { 'config_file':
   path    => '/etc/nginx/sites-available/default',
   line    => "\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n",
   after   => 'listen 80 default_server;',
+  require => Package['nginx'],
 }
 
 # start server.
@@ -68,4 +88,5 @@ file_line { 'config_file':
 exec { 'restart':
   command => 'sudo service nginx restart',
   path    => ['/usr/bin', '/bin'],
+  require => File_line['config_file'],
 }
