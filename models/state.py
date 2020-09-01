b0cc8a5dbbd95210d6940b267dc8e43a767f2ff6
FatChicken277 <1637@holbertonschool.com>
2020-09-01 12:00:41 -0500

    fixed getter method

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
 from os import getenv
@@ -12,15 +13,15 @@ class State(BaseModel, Base):
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        name = ""
 
         @property
         def cities(self):
             from models import storage
             result = []
-            dict_cities = storage.all("City")
+            dict_cities = storage.all(City)
             for key, obj in dict_cities.items():
-                if self.id == obj["state_id"]:
+                if self.id == obj.state_id:
                     result.append(obj)
             return result
-    else:
-        name = ""
